6c59aae6bc5f810e3947684535c54c8810565f89
makilyes <mak_titi@hotmail.com>
2021-10-28 13:24:31 +0200

    download

--- assets/global.js
+++ assets/global.js
@@ -712,3 +712,51 @@ class VariantRadios extends VariantSelects {
 
 customElements.define('variant-radios', VariantRadios);
 
+
+//MyVueJS script
+
+const store = Vue.reactive({
+  state: {
+    cartState: []
+  },
+
+  getCart() {
+    axios.get('/cart.js')
+        .then(response => {
+          this.state.cartState.unshift(response.data)
+        })
+        .catch(error => {
+          console.log(error)
+        })
+  }
+})
+
+const miniCartState = Vue.reactive({
+  hidden: true
+})
+
+const toggleMiniCart = {
+  openCart() {
+    miniCartState.hidden = !miniCartState.hidden
+    menuState.hidden = true
+  }
+}
+
+const menuState = Vue.reactive({
+  hidden: true
+})
+
+const toggleMenu = {
+  openMenu() {
+    menuState.hidden = !menuState.hidden
+    miniCartState.hidden = true
+  }
+}
+
+const toggleClose = {
+  closeAll() {
+    miniCartState.hidden = true
+    menuState.hidden = true
+  }
+}
+
